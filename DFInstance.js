d3e4dd9afbb04aab2a042a3f478fb4590155f97a
Thomas Frei <mail@thomasfrei.de>
2016-02-01 13:49:36 +0100

    add basic cli

--- DFInstance.js
+++ DFInstance.js
@@ -1,4 +1,22 @@
 var Promise = require('bluebird');
+var readline = require('readline'); // handle live input
+var program = require('commander'); // handle cli options
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+program
+  .version('0.0.1')
+  .option('-a, --agent-name', 'Agent name') // agent-name becomes agentName
+  .parse(process.argv);
+
+if(typeof program.agentName == 'undefined') {
+  program.agentName = 'DF';
+}
+
+// Agent config
 var eve = require('evejs');
 var DFAgent = require('./agents/DFAgent');
 
@@ -12,8 +30,22 @@ eve.system.init({
 });
 
 // create two agents
-var DF = new DFAgent('DF');
+var DF = new DFAgent(program.agentName);
 
 Promise.all([DF.ready]).then(function () {
-  console.log('DF ready');
+  console.log('agent ', program.agentName, ' ready');
 });
+
+function input(){
+  rl.question("\n>>", function(answer){
+    if(answer == 'exit') {
+      console.log('program exiting');
+      process.exit(0);
+    }
+    else {
+      console.log('you said:',answer);
+      input();
+    }
+  });
+}
+input();
